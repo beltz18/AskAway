92ddeba89cc56c550bfc90b87b0a27d2a54ecaf0
Beltz Anhxton <beltz.18kyodai@gmail.com>
2024-02-06 18:29:28 -0500

    please work now!

--- app/components/recordInterview/BodyPracticeInterview.tsx
+++ app/components/recordInterview/BodyPracticeInterview.tsx
@@ -1,117 +1,195 @@
-import { useEffect }   from 'react'
-import { useRef }      from 'react'
-import { useState }    from 'react'
-import Link            from 'next/link'
-import Webcam          from 'react-webcam'
-import Background      from './Background'
+import { useEffect } from 'react'
+import { useRef } from 'react'
+import { useState } from 'react'
+import Link from 'next/link'
+import Webcam from 'react-webcam'
+import Background from './Background'
 import { startStream } from '@a/functions'
-import { mediaRecConst, videoConst }  from '@a/global'
-import {
-  Record,
-  Stop,
-} from '@i/TakeTheInterviewIcons'
+import { mediaRecConst, videoConst } from '@a/global'
+import axios from 'axios'
+// import {
+//   Record,
+//   Stop,
+// } from '@i/TakeTheInterviewIcons'
 import {
   handleFormatTime,
 } from '@a/functions'
 
-const BodyPractice = ({ user, interview }: any) : React.JSX.Element => {
-  const [chunks, setChunks] : any = useState([])
-  const [record, setRecord] : any = useState(false)
-  const [readyP, setReadyP] : any = useState(true)
-  const [countS, setCountS] : any = useState(31)
+const BodyPractice = ({ user, interview }: any): React.JSX.Element => {
+  const [isRecording, setIsRecording] = useState(false)
+  const videoRef = useRef<null | HTMLVideoElement>(null)
+  const streamRef = useRef<null | MediaStream>(null)
+  const [downloadLink, setDownloadLink] = useState("")
+  const [isDone, setIsDone] = useState(false)
+  const streamRecorderRef = useRef<null | MediaRecorder>(null)
+  const [audioSource, setAudioSource] = useState<string>("")
+  const [videoSource, setVideoSource] = useState<string>("")
+  const [audioSourceOptions, setAudioSourceOptions] = useState<Record<string, string>[]>([])
+  const [videoSourceOptions, setVideoSourceOptions] = useState<Record<string, string>[]>([])
+  const [error, setError] = useState<null | Error>(null)
+  const chunks = useRef<any[]>([])
 
-  const webCamVideoRef : any = useRef(null)
-  const mediaRecorder  : any = useRef(null)
+  function startRecording() {
+    if (isRecording) return
+    if (!streamRef.current) return
 
-  const handleData = ({ data }: any) => setChunks(data)
-
-  const stopStream = () => {
-    mediaRecorder.current.stop()
-    setCountS(31)
-    setRecord(false)
-    setReadyP(false)
+    streamRecorderRef.current = new MediaRecorder(streamRef.current)
+    streamRecorderRef.current.start()
+    streamRecorderRef.current.ondataavailable = async (event: BlobEvent) => {
+      if (chunks.current) {
+        chunks.current.push(event.data)
+        setIsDone(true)
+      }
+    }
+    setIsRecording(true)
   }
 
-  const HandlePlayVideo = () => {
-    const url : any = URL.createObjectURL(chunks)
-    const mp4 : any = document?.querySelector('#videoRecorded')
-    mp4.src = url
-    mp4.play()
-    setChunks([])
-  }
-
-  useEffect(() => { if (chunks?.size) HandlePlayVideo() }, [chunks])
   useEffect(() => {
-    let interval : any
+    if (isRecording) return
+    if (isDone) {
+      setIsDone(false)
+      const blob = new Blob(chunks.current, { type: "video/x-matroskacodecs=avc1,opus" })
+      setDownloadLink(URL.createObjectURL(blob))
+      chunks.current = []
 
-    if (record && countS > 0)
-      interval = setInterval(() => setCountS((secs: number = 0) => secs - 1), 1000)
-    else clearInterval(interval)
-    
-    return () => clearInterval(interval)      
-  }, [record])
+      const data = {
+        file: blob,
+        key: 'd8b6c045c2d707256ad6822569dcd55bc26672cb',
+        tokenInt: 'f881885d4f82269ba2fe7b100db8a8af',
+        question: 'sdf',
+        questionID: 'ed6efba41cf19f34f90660eb5903178d',
+      }
 
-  countS == 0 && record && stopStream()
+      axios.post('https://localhost:4043/v2/upload/video/123456789', data, {
+        headers: { 'Content-Type': 'multipart/form-data' }
+      })
+        .then((res)  => console.log(res))
+        .catch((err) => console.log(err))
+    }
+  }, [isRecording, isDone])
+
+  function stopRecording() {
+    if (!streamRecorderRef.current) return
+    streamRecorderRef.current.stop()
+    setIsRecording(false)
+  }
+
+  useEffect(function () {
+    async function prepareStream() {
+      function gotStream(stream: MediaStream) {
+        streamRef.current = stream
+        if (videoRef.current) videoRef.current.srcObject = stream
+      }
+
+      async function getStream() {
+        if (streamRef.current) {
+          streamRef.current.getTracks().forEach((track) => {
+            track.stop()
+          })
+        }
+        const constraints = {
+          audio: { deviceId: audioSource !== "" ? { exact: audioSource } : undefined },
+          video: { deviceId: videoSource !== "" ? { exact: videoSource } : undefined },
+        }
+        try {
+          const stream = await navigator.mediaDevices.getUserMedia(constraints)
+          gotStream(stream)
+        } catch (err: any) { setError(err) }
+      }
+
+      function getDevices() { return navigator.mediaDevices.enumerateDevices() }
+
+      function gotDevices(deviceInfos: MediaDeviceInfo[]) {
+        const audioSourceOptions = []
+        const videoSourceOptions = []
+
+        for (const deviceInfo of deviceInfos) {
+          if (deviceInfo.kind === "audioinput") {
+            audioSourceOptions.push({
+              value: deviceInfo.deviceId,
+              label: deviceInfo.label || `Microphone ${deviceInfo.deviceId}`,
+            })
+          } else if (deviceInfo.kind === "videoinput") {
+            videoSourceOptions.push({
+              value: deviceInfo.deviceId,
+              label: deviceInfo.label || `Camera ${deviceInfo.deviceId}`,
+            })
+          }
+        }
+        setAudioSourceOptions(audioSourceOptions)
+        setVideoSourceOptions(videoSourceOptions)
+      }
+
+      await getStream()
+      const mediaDevices = await getDevices()
+      gotDevices(mediaDevices)
+    }
+    prepareStream()
+  }, [])
 
   return (
     <>
       <div className="w-full h-full flex flex-col relative">
         <Background
           message={'Prepare your equipement'}
-          current={ 2 }
-          length={ 2 }
+          current={2}
+          length={2}
         />
 
         <div className='absolute left-0 w-full h-[90vh] flex flex-col items-center justify-center gap-6 mt-6'>
           <div className='w-[90%] md:w-[40%] flex flex-col bg-white p-2 shadow-2xl rounded-lg'>
-            {
-              !readyP
-                ?
-              (
-                <video
-                  id='videoRecorded'
-                  autoPlay
-                  controls
-                  preload='auto'
-                  className='w-full rounded-lg bg-black'
-                  data-setup='{}'
-                />
-              )
-                :
-              (
-                <Webcam
-                  mirrored={ true }
-                  ref={ webCamVideoRef }
-                  videoConstraints={ videoConst }
-                />
-              )
-            }
-            <div className='p-2 h-[25%] flex gap-6 items-center justify-center'>
-              <button
-                className='bg-[#D9D9D961] w-[100px] px-3 py-2 rounded-md flex flex-col items-center justify-center'
-                onClick={() => startStream(mediaRecConst, webCamVideoRef, mediaRecorder, handleData, [setReadyP, setRecord]) }
-                disabled={ record }
+            <div>
+              <select
+                id="videoSource"
+                name="videoSource"
+                value={videoSource}
+                onChange={(e: any) => setVideoSource(e.target.value)}
               >
-                <Record color={ !record ? '#EA830A' : '#525252' } />
-                Record
+                {
+                  videoSourceOptions.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))
+                }
+              </select>
+            </div>
+            <div>
+              <select
+                id="audioSource"
+                name="audioSource"
+                value={audioSource}
+                onChange={(e: any) => setAudioSource(e.target.value)}
+              >
+                {
+                  audioSourceOptions.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))
+                }
+              </select>
+            </div>
+            <div>
+              <video ref={videoRef} autoPlay muted playsInline></video>
+            </div>
+            <div>
+              {downloadLink && <video src={downloadLink} controls></video>}
+              {downloadLink && (
+                <a href={downloadLink} download="file.mp4">
+                  Descargar
+                </a>
+              )}
+            </div>
+            <div>
+              <button onClick={startRecording} disabled={isRecording}>
+                Grabar
               </button>
-              {
-                countS < 31 && countS >= 0 && (
-                  <div className='flex flex-col items-center justify-between'>
-                    Recording
-                    <span>{ handleFormatTime(countS) }</span>
-                  </div>
-                )
-              }
-              <button
-                className='bg-[#D9D9D961] w-[100px] px-3 py-2 rounded-md flex flex-col items-center justify-center'
-                onClick={ stopStream }
-                disabled={ !record }
-              >
-                <Stop color={ record ? '#DC0303' : '#525252' } />
-                Stop
+              <button onClick={stopRecording} disabled={!isRecording}>
+                Parar
               </button>
             </div>
+            <div>{error && <p>{error.message}</p>}</div>
           </div>
 
           <div className='w-[90%] md:w-[40%] flex items-center justify-center bg-white p-4 shadow-2xl rounded-lg'>
--- pages/interviews/[interviewId]/index.tsx
+++ pages/interviews/[interviewId]/index.tsx
@@ -10,7 +10,7 @@ const VideoInterviewTable = () : React.JSX.Element => {
       <h1>Interview</h1>
 
       <video controls={true} preload='metadata' playsInline>
-        <source src='https://storage.googleapis.com/ask-away-production/videos/AVANZA-TECH/GERENCIA-GENERAL/ASISTENCIA-A-GERENCIA-Y-PRESIDENCIA/Jessica_Palacios__1ed63e/1n7MvwpwDNDzA9FFiLjmrp.mp4' type='video/mp4' />
+        <source src='https://storage.googleapis.com/ask-away-development/videos/d8b6c045c2d707256ad6822569dcd55bc26672cb/1b4111e5691b79225c1eaacc71bbe6/Hola_Hola-3__b8a8af/c4qLMXCa6PmjiVPJhXBWsU.mp4' type='video/mp4' />
       </video>
 
       {/* <video
