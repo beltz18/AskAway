08a5eaa1d8acc8d896c00f79cd715ae92d571335
Beltz Anhxton <beltz.18kyodai@gmail.com>
2024-02-08 21:08:27 -0500

    as far as i can tell, i think this MTF works

--- pages/interviews/[interviewId]/index.tsx
+++ pages/interviews/[interviewId]/index.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import { useState }  from 'react'
+import { useRef }    from 'react'
+import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 
-const VideoInterviewTable = () : React.JSX.Element => {
+async function loadVideoFromURL() {
+  try {
+    const response : any = await fetch('https://storage.googleapis.com/ask-away-development/videos/d8b6c045c2d707256ad6822569dcd55bc26672cb/1b4111e5691b79225c1eaacc71bbe6/Hola_Hola-3__b8a8af/c4qLMXCa6PmjiVPJhXBWsU.mp4')
+    const arrayBuffer = await response.arrayBuffer()
+    const blob = new Blob([arrayBuffer], { type: 'video/mp4' })
+
+    return blob
+  } catch (error) {
+    console.error('Error fetching video:', error)
+  }
+}
+
+const VideoInterviewTable = ({ video }: any) : React.JSX.Element => {
   const router : any    = useRouter()
   const { interviewId } = router.query
-  
+
+  const videoRef : any = useRef(null)
+
+  useEffect(() => {
+    const loadVideo = async () => {
+      try {
+        const blob : any = await loadVideoFromURL()
+        console.log(blob)
+        videoRef.current.src = URL.createObjectURL(blob)
+        videoRef.current.load()
+      } catch (error) { console.error('Error loading video:', error) }
+    }
+
+    loadVideo()
+  }, [])
+
   return (
     <>
       <h1>Interview</h1>
@@ -13,7 +42,7 @@ const VideoInterviewTable = () : React.JSX.Element => {
         controls={ true }
         playsInline
         preload='metadata'
-        src={`https://test.askaway.io/v2/get/video/:video`}
+        ref={ videoRef }
       />
     </>
   )
